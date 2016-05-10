0f64c27b19b8f761a445033673c3aef9234f37ee
Flavio <cavolate09@gmail.com>
2016-05-10 14:30:01 +0200

    merge Tex

--- Utility.py
+++ Utility.py
@@ -6,8 +6,8 @@ import socket
 import threading
 
 class Utility:
-    IPv4_TRACKER = "172.030.007.010"
-    IPv6_TRACKER = "fc00:0000:0000:0000:0000:0000:0007:0010"
+    IPv4_TRACKER = "172.030.007.001"
+    IPv6_TRACKER = "fc00:0000:0000:0000:0000:0000:0007:0001"
     IP_TRACKER = IPv4_TRACKER+'|'+IPv6_TRACKER
 
     PORT_TRACKER = 3000
@@ -18,8 +18,8 @@ class Utility:
 
     PORT_MY = 12345
 
-    PATHDIR = '/home/marco/seedfolder/'
-    PATHTEMP = '/home/marco/seedfolder/temp/'
+    PATHDIR = '/home/flavio/Scrivania/File/'
+    PATHTEMP = '/home/flavio/Scrivania/File/temp/'
 
     ## variabili condivise in piu' parti del programma
     LEN_PART = 262144
